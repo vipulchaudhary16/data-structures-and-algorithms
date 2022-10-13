876458134b1ec3c10dfef97ee24de2ab46582452
Aman Kumar <61074175+amanjaiswa@users.noreply.github.com>
2022-10-14 01:33:41 +0530

    Create Reverse String.cpp
    I have written the very easy code of reverse String in CPP

---
+++ 450 By Love Babbar/String/Reverse String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int start= 0;
+        int end = s.size()-1;
+        
+        while (start<end){
+            swap(s[start++], s [end--]);
+        }
+        
+    }
+};
